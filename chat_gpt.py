04a7b9b034a58a02157049cf39cac834b00d606a
hitanon <hitanon>
2023-06-08 23:34:33 +0500

    added cyclic change of api keys for chatgpt

--- chat_gpt.py
+++ chat_gpt.py
@@ -2,21 +2,27 @@ import json
 import openai
 
 
-openai.api_key = open("resources/api_key_chat_gpt.txt", "r").read().strip('\n')
+def load_api_key(line_number):
+    with open("resources/api_key_chat_gpt.txt", "r") as file:
+        for i, line in enumerate(file):
+            if i == line_number - 1:
+                openai.api_key = line.strip('\n')
+                break
 
 
 class ChatGPT:
-    def __init__(self, prompt_file):
+    def __init__(self, prompt_file, number_of_key):
         self.prompt = None
         self.prompt_file = prompt_file
         self.insert_place = '<FILMS>'
         self._read_prompt_from_file()
+        load_api_key(number_of_key)
 
     def send_request(self):
         response = openai.ChatCompletion.create(
             model="gpt-3.5-turbo",
             messages=[{"role": "user", "content": self.prompt}],
-            temperature=0.1
+            temperature=0.08
         )
         response_text = response['choices'][0]['message']['content']
         print(response_text)
@@ -30,7 +36,4 @@ class ChatGPT:
     def _read_prompt_from_file(self):
         with open(self.prompt_file, 'r') as f:
             self.prompt = f.read()
-
-    def save_response(self):
-        pass
 
--- main.py
+++ main.py
@@ -1,23 +1,27 @@
 import argparse
 import time
 
-from chat_gpt import ChatGPT
+from chat_gpt import ChatGPT, load_api_key
 from film_searcher import FilmSearcher
 import utils.json_utils as utils
 
 if __name__ == '__main__':
+
     parser = argparse.ArgumentParser()
     parser.add_argument('--cnt_pages', type=int, default=10,
                         help='Number of pages to process')
     args = parser.parse_args()
 
     CNT_PAGES = args.cnt_pages
+    number_api_key = 1
+    count_api_keys = 5
+    count_errors = 0
 
     film_searcher = FilmSearcher(20)
-    chat_gpt = ChatGPT("resources/prompt.txt")
+    chat_gpt = ChatGPT("resources/prompt.txt", number_api_key)
 
     cnt_pages = film_searcher.page + CNT_PAGES
-    count_errors = 0
+
     while film_searcher.page < cnt_pages:
         if count_errors >= 10:
             break
@@ -33,6 +37,9 @@ if __name__ == '__main__':
         except Exception:
             print('Error with open-ai API')
             time.sleep(60)
+            number_api_key += 1
+            number_api_key %= 5
+            load_api_key(number_api_key)
             count_errors += 1
             continue
         utils.add_ids(response, films)
